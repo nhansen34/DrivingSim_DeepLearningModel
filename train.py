cc07f469da9293d4f68713a1a9b8ab2e96aa4179
Nick Hansen <nickh34@live.com>
2025-03-06 14:37:17 -0800

    Get python scripts so jobs can be executed thru terminal

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from dataset import load_datasets 
-from model import get_model  
+from notebooks import load_datasets 
+from notebooks import get_model  
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
